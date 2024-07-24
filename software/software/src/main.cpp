6e1ff0e464637803b10654e2b0f1b5b692dc03b1
rmingon <ronanmingon69550@gmail.com>
2024-07-24 22:59:23 +0200

    add motor test

--- software/software/src/main.cpp
+++ software/software/src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 
-#define SENSITIVITY 254
+#define SENSITIVITY 100
 
 // IR PIN LEFT TO RIGHT
 #define IR0 PIN_PF0
@@ -19,21 +19,45 @@
 // MX1208
 #define MOTOR_L_A PIN_PH3
 #define MOTOR_L_B PIN_PH4
-#define MOTOR_R_B PIN_PH5
+#define MOTOR_R_A PIN_PH5
 #define MOTOR_R_B PIN_PH6
 
 // LEDS BOTTOM
 #define LEDS PIN_PK2
 
+#define BUTTON PIN_PK3
+
 // 
 
 void setup() {
   pinMode(PIN_PA3, OUTPUT);
-  pinMode(PIN_PF1, INPUT);
+  pinMode(BUTTON, INPUT);
+
+  pinMode(LEDS, OUTPUT);
+  
+  pinMode(MOTOR_L_A, OUTPUT);
+  pinMode(MOTOR_L_B, OUTPUT);
+  pinMode(MOTOR_R_A, OUTPUT);
+  pinMode(MOTOR_R_B, OUTPUT);
+  analogWrite(MOTOR_L_A, 0);
+  analogWrite(MOTOR_L_B, 0);
+  analogWrite(MOTOR_R_A, 0);
+  analogWrite(MOTOR_R_B, 0);
+  
+  digitalWrite(LEDS, HIGH);
+  pinMode(IR3, INPUT);
+  pinMode(IR4, INPUT);
+  pinMode(IR5, INPUT);
+  pinMode(IR6, INPUT);
 }
 
 void loop() {
-  int value = analogRead(PIN_PF1);
+  if (digitalRead(BUTTON)) {
+    delay(300);
+    analogWrite(MOTOR_L_A, 40);
+    analogWrite(MOTOR_R_A, 40);
+  }
+  int value = analogRead(IR3);
   if (value < SENSITIVITY) {
     digitalWrite(PIN_PA3, LOW);
   } else {
